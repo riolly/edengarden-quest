aa80221d827098c7b57157195cf873858f54b7c0
riolly <alberto.riolly@gmail.com>
2023-07-05 14:34:17 +0700

    clerk key

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_aW52aXRpbmctcm9vc3Rlci0zNi5jbGVyay5hY2NvdW50cy5kZXYk";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
